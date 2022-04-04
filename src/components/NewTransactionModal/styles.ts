f5358128cc0a5d508c247718c57b598592e9752b
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-04-04 19:15:47 -0300

    Adicionado seleção e estilo de botoes selecionados

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
   h2 {
@@ -55,32 +55,43 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
+`
 
-    background: transparent;
+interface RadioBox {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d',
+}
 
-    transition: border-color 0.2s;
+export const RadioBox = styled.button<RadioBox>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
 
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
+  background: ${({ isActive, activeColor }) => isActive ? transparentize(0.9, colors[activeColor]) : 'transparent'};
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `
